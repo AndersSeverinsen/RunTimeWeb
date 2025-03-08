56648cc2f4ab91146cb48eba9bdbf5bdd876edc5
Anders Bruun Severinsen <202204885@post.au.dk>
2025-03-08 12:46:46 +0100

    Updates

--- _app/immutable/entry/start.DirNdxk8.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DQ2tM1Jk.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/2.CedN1cSN.js
@@ -0,0 +1,6 @@
+import{t as o,b as i}from"../chunks/dWOrwwKi.js";import"../chunks/BoIXh8xF.js";import{O as r,P as d,Q as l,R as s,S as u}from"../chunks/iFupyO9f.js";import{s as g}from"../chunks/CjK-ldRF.js";import{b as m}from"../chunks/zNPZVjvF.js";var h=o(`<div class="welcome-text svelte-dgatxu"><h1 class="svelte-dgatxu">Welcome to</h1> <img id="bannerLogo" alt="Runtime logo" style="width:300px;" class="svelte-dgatxu"> <p class="svelte-dgatxu">Pushing limits — on the track and in the terminal.</p></div> <div class="content svelte-dgatxu"><div class="short-introduction svelte-dgatxu"><h3 class="svelte-dgatxu">RunTime</h3> <p>RunTime is the running club made by CS students for CS students. The
+            goal is to get students together through running and this way meet
+            more people sharing the same studies. We wan't to create an
+            environment where everyone feels seen and enjoys being a part of. If
+            this sounds interesting but you have any questions, feel free to
+            reach out through the contact page or come and enjoy a nice run!</p> <p id="end-words" class="svelte-dgatxu">Hope to see you!</p></div></div>`,1);function b(a){var e=h(),t=r(e),n=d(l(t),2);g(n,"src",`${m??""}/logo/Banner_uBaggrund.png`),s(2),u(t),s(2),i(a,e)}export{b as component};
